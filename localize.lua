8a9ae86ce5f142a10f1f36f543ebab93f13ba8f7
SnkaeSael <66127672+SnkaeSael@users.noreply.github.com>
2021-08-17 11:01:31 -0400

    Added lua

---
+++ localize.lua
@@ -0,0 +1,35 @@
+marioBool = false
+file = io.open("NAME.txt", "a")
+
+function updateMario()
+	marioBool = false
+	smallMario = memory.readbyte(1876)
+	
+	if (memory.readbyte(181) == 1 and memory.readbyte(14) == 8) then
+		marioBool = true
+		return
+	end
+	
+	if (memory.readbyte(14) ~= 0) then
+		for x = 0, 255 do
+			for y = 0, 239 do
+				temp1, temp2, temp3, temp4 = emu.getscreenpixel(x, y, true)
+				if (temp4 == 24) then
+					marioBool = true
+				end
+			end
+		end
+	end
+end
+
+while (true) do
+	if taseditor.engaged() then
+		updateMario()
+		if marioBool then
+			file:write(emu.framecount() .. ":1," .. memory.readbyte(941) .. "," .. ((256 * (memory.readbyte(181) - 1)) + memory.readbyte(206) + (smallMario * 16)), "\n")
+		else
+			file:write(emu.framecount() .. ":0", "\n")
+		end
+	end
+	emu.frameadvance()
+end
