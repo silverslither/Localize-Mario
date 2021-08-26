28d3975948bb37f8fb4745a73dddcf83f9eebe11
SnkaeSael <66127672+SnkaeSael@users.noreply.github.com>
2021-08-26 14:27:34 -0400

    Added Camera Localization Script

---
+++ camera.lua
@@ -0,0 +1,8 @@
+file = io.open("NAME.txt", "a")
+
+while (true) do
+	if (taseditor.engaged() and memory.readbyte(14) ~= 0) then
+		file:write(emu.framecount() .. ":" .. (memory.readbyte(1887) + 1) .. "-" .. (memory.readbyte(1888) + 1) .. "," .. (256*memory.readbyte(109) + memory.readbyte(134) - memory.readbyte(941)), "\n")
+	end
+	emu.frameadvance()
+end
